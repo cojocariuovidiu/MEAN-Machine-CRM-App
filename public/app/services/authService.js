89aa45f5a70a5ce724506b2f5124a0b5af843a81
Giuseppe DiBella <gd46@njit.edu>
2015-07-03 13:27:19 -0400

    Began setting up frontend

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -45,7 +45,8 @@ angular.module('authService', [])
 
     authFactory.getUser = function() {
       if(AuthToken.getToken()){
-        return $http.get('/api/me');
+        return $http.get('/api/me', {
+          cache: true });
       } else {
         return $q.reject({ message: 'User has no token'});
       }
